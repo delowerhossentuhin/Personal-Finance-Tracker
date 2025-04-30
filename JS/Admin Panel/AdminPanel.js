234dd223c9d8947f210ed7450149892dd86def1c
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-04-30 07:20:10 +0600

    Admin Dashboard Updated

--- JS/Admin Panel/AdminPanel.js
+++ JS/Admin Panel/AdminPanel.js
@@ -1,7 +1,14 @@
-allPendingIcon=document.getElementById('all-Pending')
-pendingUser=document.getElementById("pendingUser")
-addUser=document.getElementById("addUser")
 document.addEventListener("DOMContentLoaded", ()=>{
+    allPendingIcon=document.getElementById('all-Pending')
+    pendingUser=document.getElementById("pendingUser")
+    addUser=document.getElementById("addUser")
+    settingBTN=document.getElementById("setting")
+    settingPanel=document.getElementById('settingPanel')
+    profileDetails=document.getElementById('profileDetails')
+    adminDetails=document.getElementById('adminDetails')
+    DashboardBTN=document.getElementById("Dashboard")
+    DashboardPanel=document.getElementById("dashboardPanel")
+    //
     allPendingIcon.addEventListener('click',()=>{
         if (pendingUser.style.display === 'none' || pendingUser.style.display === '') {
             pendingUser.style.display = 'flex'; 
@@ -14,4 +21,36 @@ document.addEventListener("DOMContentLoaded", ()=>{
             addUser.style.display = 'none';
         }
     })
+    profileDetails.addEventListener('click',()=>{
+        if(adminDetails.style.display==='none'||adminDetails.style.display===''){
+            adminDetails.style.display="block"
+            
+        }
+        else{
+            adminDetails.style.display='none'
+        }
+    }) 
+
+
+
+    //Click Setting btn and open setting panel
+    settingBTN.addEventListener('click', () => {
+        if (settingPanel.style.display === 'none') {
+            settingPanel.style.display = 'block';
+            DashboardPanel.style.display="none"
+        } else {
+            settingPanel.style.display = 'none';
+        }
+    });   
+    
+    DashboardBTN.addEventListener('click',()=>{
+        if(DashboardPanel.style.display==='none'||DashboardPanel.style.display===""){
+            DashboardPanel.style.display="block"
+            settingPanel.style.display = 'none';
+        }
+        else {
+            DashboardPanel.style.display="none"
+        }
+    })
+
 })
